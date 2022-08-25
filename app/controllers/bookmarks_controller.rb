ae21f0277c0dc9fb684998726ff43afed55118a5
Olivia Patterson <oliviapatterson123@live.com>
2022-08-25 14:46:22 +0100

    almost

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,26 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(params[:list_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
